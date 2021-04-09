bcd475a703aee6386eb80f7243c54683a2dcd979
KevinHARAMENDY <kev17.haramendy@gmail.com>
2021-04-09 09:28:40 +0200

    Fix création compte -> sauvegarde champs

--- src/servlet/CreationCompte.java
+++ src/servlet/CreationCompte.java
@@ -6,41 +6,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import bll.UserManager;
 import bo.Utilisateurs;
 
-@WebServlet(
-		urlPatterns= {
-						"/CreationCompte",
-						"/supprimerProfil"
-		})
+@WebServlet("/CreationCompte")
 public class CreationCompte extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	if(request.getServletPath().equals("/CreationCompte")) {
-    		this.getServletContext().getRequestDispatcher("/WEB-INF/creationProfil.jsp").forward(request, response);
-    	}
-    	else if(request.getServletPath().equals("/supprimerProfil")) {
-            // je récupère le pseudo dans la session en cours
-    		HttpSession session = request.getSession();
-            Utilisateurs utilisateurs = (Utilisateurs) session.getAttribute("User");
-            String pseudo = utilisateurs.getPseudo();
-            
-            // je supprime le profil et invalide sa session
-    		UserManager userMan = new UserManager();
-    		
-            if ( pseudo != null ) {
-        		userMan.supprimerUser(pseudo);
-        		session.invalidate();
-        		this.getServletContext().getRequestDispatcher("/WEB-INF/accueil.jsp").forward(request, response);
-            }else {
-        		this.getServletContext().getRequestDispatcher("/WEB-INF/error.jsp").forward(request, response);
-            }
-    	}
-    	
+		this.getServletContext().getRequestDispatcher("/WEB-INF/creationProfil.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -64,6 +39,17 @@ public class CreationCompte extends HttpServlet {
 			} else {
 				request.setAttribute("erreur", "Veuillez remplir tous les champs");
 			}
+			request.setAttribute("pseudo", request.getParameter("pseudo"));
+			request.setAttribute("nom", request.getParameter("nom"));
+			request.setAttribute("prenom", request.getParameter("prenom"));
+			request.setAttribute("email", request.getParameter("email"));
+			request.setAttribute("tel", request.getParameter("tel"));
+			request.setAttribute("rue", request.getParameter("rue"));
+			request.setAttribute("cp", request.getParameter("cp"));
+			request.setAttribute("ville", request.getParameter("ville"));
+			request.setAttribute("mdp", request.getParameter("mdp"));
+			request.setAttribute("confirmation", request.getParameter("confirmation"));
+			
 			this.getServletContext().getRequestDispatcher("/WEB-INF/creationProfil.jsp").forward(request, response);
 		}
 	}
